96264ac5dbfad0db67afd3a9b7c691e3ae95dcd4
Enzo Gabriel Previtale Silva <49513408876@>
2025-08-11 13:55:05 -0300

    Exercício 2

---
+++ exercicios/exercicio2.js
@@ -0,0 +1,3 @@
+let num = [Number(prompt("Digite um número.")), Number(prompt("Digite outro número.")), Number(prompt("Digite mais um número."))];
+
+alert("O maior número é " + Math.max(...num) + ".");
