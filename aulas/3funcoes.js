2b415c5c487ff948482697ef81669ace6d6d2cb7
Enzo Gabriel Previtale Silva <49513408876@>
2025-08-11 16:35:50 -0300

    Funções

---
+++ aulas/3funcoes.js
@@ -0,0 +1,13 @@
+// Funções
+function func() {
+    
+}
+
+// Funções anônimas
+let soma = function(n1, n2) {
+    return n1 + n2;
+}
+alert(soma(2, 3));
+
+// Arrow functions
+let arrow = (n1, n2) => n1 + n2;
