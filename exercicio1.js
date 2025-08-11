360f55f17fba1c7e6a311901fd3a31990fa8f82c
Enzo Gabriel Previtale Silva <49513408876@>
2025-08-11 13:50:51 -0300

    Exercício 1

---
+++ exercicio1.js
@@ -0,0 +1,15 @@
+let num = Number(prompt("Digite um número."));
+
+if(num % 2 === 0) {
+    alert("O número é par.");
+} else {
+    alert("O número é ímpar.");
+}
+
+if(num % 3 == 0) {
+    alert("O número é múltiplo de três.");
+}
+
+if(num % 5 == 0) {
+    alert("O número é múltiplo de cinco.");
+}
