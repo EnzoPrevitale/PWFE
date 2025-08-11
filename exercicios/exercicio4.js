0c204e6ba0a12df8556ec0bd3303c0f21d1f37cc
Enzo Gabriel Previtale Silva <49513408876@>
2025-08-11 14:10:17 -0300

    Exercício 4

--- exercicios/exercicio4.js
+++ exercicios/exercicio4.js
@@ -1,5 +1,11 @@
-let opcao = Number(prompt("[1] Celsius -> Fahrenhit \n [2] Fahrenheit -> Celsius"));
+let opcao = Number(prompt("[1] Celsius -> Fahrenheit \n [2] Fahrenheit -> Celsius"));
+let temperatura = Number(prompt("Digite a temperatura."));
+let temperatura_nova;
 
 if(opcao == 1) {
-    
+    temperatura_nova = (temperatura * 9/5) + 32;
+    alert(temperatura + "°C = " + temperatura_nova + "°F");
+} else if(opcao == 2) {
+    temperatura_nova = (temperatura - 32) / (9/5);
+    alert(temperatura + "°F = " + temperatura_nova + "°C");
 }
