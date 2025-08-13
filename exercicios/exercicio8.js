59f95691947e8ef47f4d52cc32a9cba12a9609cd
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-08-12 21:55:13 -0300

    Exercício 8

--- exercicios/exercicio8.js
+++ exercicios/exercicio8.js
@@ -4,4 +4,10 @@ if(ano % 100 != 0) {
     if(ano % 4) {
         alert("O ano é bissexto.");
     }
+} else {
+    if(ano % 400 == 0) {
+        alert("O ano é bissexto.");
+    } else {
+        alert("O ano não é bissexto.");
+    }
 }
